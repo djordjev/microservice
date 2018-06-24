63a838d5b70f4846c96ab40ba4142541295e0e21
= <=>
2018-06-24 22:48:34 +0200

    React + babel + webpack + eslint + prettier

---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/index.html",
+      filename: "index.html"
+    })
+  ]
+};
